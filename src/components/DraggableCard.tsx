77c7216125904c9b5e5114a55441fbc682a777c3
wannabeing <wannabeing@naver.com>
2022-07-08 03:05:51 +0900

    EDIT board/card-componenet CSS

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.cardColor};
-  padding: 10px 10px;
+  background-color: ${(props) =>
+    props.isDragging ? props.theme.textColor : props.theme.cardColor};
+  color: ${(props) => (props.isDragging ? props.theme.cardColor : null)};
+  font-weight: ${(props) => (props.isDragging ? 600 : null)};
+  padding: 15px 10px;
   border-radius: 7px;
-  margin-bottom: 7px;
+  margin-bottom: 10px;
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.0784313725);
 `;
 
 interface ICardProps {
@@ -20,11 +24,12 @@ interface ICardProps {
 function DraggableCard({ cardName, index }: ICardProps) {
   return (
     <Draggable key={cardName} draggableId={cardName} index={index}>
-      {(magic) => (
+      {(magic, snapshot) => (
         <Card
           ref={magic.innerRef}
           {...magic.draggableProps}
           {...magic.dragHandleProps}
+          isDragging={snapshot.isDragging}
         >
           <span>🔥</span>
           {cardName.toUpperCase()}
--- src/components/DroppableBoard.tsx
+++ src/components/DroppableBoard.tsx
@@ -3,20 +3,23 @@ import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.0784313725);
 `;
 
-const Board = styled.div`
+const Board = styled.div<{ isDraggingOver: boolean }>`
   background-color: ${(props) => props.theme.boardColor};
-  padding: 20px 10px;
+  padding: 0px 10px;
   padding-top: 2px;
   border-radius: 7px;
   width: 200px;
-  min-height: 150px;
+  flex-grow: 1;
 `;
 const Title = styled.h2`
   display: flex;
@@ -26,6 +29,7 @@ const Title = styled.h2`
   background-color: ${(props) => props.theme.textColor};
   color: ${(props) => props.theme.cardColor};
   padding: 5px;
+  border-radius: 5px;
 `;
 
 interface IBoardProps {
@@ -39,8 +43,12 @@ function DroppableBoard({ board, boardName }: IBoardProps) {
       <Title>{boardName.toUpperCase()}</Title>
       <hr />
       <Droppable droppableId={boardName}>
-        {(magic) => (
-          <Board ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, snapshot) => (
+          <Board
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
             {board.map((cardName, index) => (
               <DraggableCard key={cardName} cardName={cardName} index={index} />
             ))}
